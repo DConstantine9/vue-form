325465ca7667acaf2ecc3499a8046f1a32cd6ac8
DConstantine9 <constantinedeniskkin@gmail.com>
2020-08-10 14:26:24 +0300

    fix

---
+++ dist/precache-manifest.ffa2f531f7134f29ffc0c19c6640256a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a224e68276b230b4671d",
+    "url": "/vue-form/css/app.bc5f69d3.css"
+  },
+  {
+    "revision": "77b72c5686d882c8d7e37b2746aba00a",
+    "url": "/vue-form/index.html"
+  },
+  {
+    "revision": "a224e68276b230b4671d",
+    "url": "/vue-form/js/app.6dcc1b07.js"
+  },
+  {
+    "revision": "896ec6cee81662e46b0f",
+    "url": "/vue-form/js/chunk-vendors.e46c39f7.js"
+  },
+  {
+    "revision": "bc2132b49139c2628dd9f20e6ca4cb87",
+    "url": "/vue-form/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-form/robots.txt"
+  }
+]);
---
+++ dist/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/vue-form/precache-manifest.ffa2f531f7134f29ffc0c19c6640256a.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "vue-form"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
